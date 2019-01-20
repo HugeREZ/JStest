dc304ca878d2b91c9828d77fc74a071fe03475fd
Huge REZ <mementodie@gmail.com>
2019-01-20 14:49:51 +0300

    First

---
+++ README.md
@@ -0,0 +1,5 @@
+# JSquest
+SImple ex for me
+He hey hey
+/n 
+I did fix it 
---
+++ TESTjs.js
@@ -0,0 +1,4 @@
+var mass = ["1q","2q","3q"];
+var answer = ['1','2','3','4'];
+var result = document.getElementById("result");
+
